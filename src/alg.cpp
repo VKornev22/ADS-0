4c6166216a91d721e12a26a0f081771a7fcc3572
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-03-02 00:26:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,10 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
-    while ((b != 0) && (a != 0)) {
-	        if (a > b)
-		       a = a - b;
-		else
-		       b = b - a;
-	}
-	return (a+b);
+    while (a != b)	    
+    if (a > b) 
+    a -= b;
+    else 	   
+        b -= a;
+    return a;
 }
